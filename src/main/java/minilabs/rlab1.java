840117ccfa5cea69ade8c7bc0e2196603b56ec5e
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2021-08-31 10:02:35 -0700

    Raq's minilab 1 (updated)

--- src/main/java/minilabs/rlab1.java
+++ src/main/java/minilabs/rlab1.java
@@ -1,4 +1,25 @@
 package minilabs;
 
+
 public class rlab1 {
+        public static void main(String[] args){
+            // Intializing all the variables
+            int grade1 = 85;
+            int grade2 = 95;
+            int grade3 = 78;
+            int grade4 = 65;
+            int grade5 = 100;
+
+            // implementing all the variables
+            double grade6 = (grade1 + grade2)/2;
+            double grade7 = (grade3 + grade4)/2;
+            double grade8 = (grade6 + grade7+grade5)/3;
+
+            System.out.println("The Average Grade is: " + grade8);
+            System.out.println("The Average Grade of the first two scores is: " + grade6);
+            System.out.println("The Average Grade of the 3rd and 4th two scores is: " + grade7);
+
+
+
+        }
 }
