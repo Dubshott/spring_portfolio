0343c3e9073db97e0d1866420fe0ff544ca5e16e
Uhpachee <sea7wa@gmail.com>
2021-09-07 13:28:24 -0700

    made minilab, changed a few html file names, updated navbar

---
+++ src/main/java/minilabs/kian/minilab2.java
@@ -0,0 +1,33 @@
+package minilabs.kian;
+
+public class minilab2 {
+    static void rankings(
+    int ties,
+    int wins,
+    int losses){
+        String stats = "Your team has " + ties + " ties, " + wins + " wins, and " + losses + " losses.";
+        System.out.println(stats);
+        if (losses == 0) {
+            System.out.println("Your team is destroying the competition!");
+        }
+        else if (losses < 3) {
+            System.out.println("Your team is doing alright!");
+        }
+        else if (losses < 5) {
+            System.out.println("Your team isn't doing very good.");
+        }
+        else {
+            System.out.println("Your team sucks. Wow.");
+        }
+
+
+    }
+
+    public static void main(String[] args){
+        rankings(0, 1, 8);
+        rankings(1, 3, 4);
+        rankings(3, 5, 1);
+        rankings(16, 32, 0);
+
+    }
+}
