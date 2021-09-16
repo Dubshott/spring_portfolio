7a3e7133c40ea50a1a4c33fed1420edbb7b33935
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2021-09-16 09:10:38 -0700

    Raquib Alam's Minilab 4 code! Lets get it Boys!

---
+++ src/main/java/minilabs/raquib/rlab3.java
@@ -0,0 +1,40 @@
+package minilabs.raquib;
+import java.util.Scanner;
+
+public class rlab3 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter your name, height (inches) and vertical (inches)");
+
+        // String input
+        String name = myObj.nextLine();
+
+        // Numerical input
+        double height = myObj.nextDouble();
+        double vertical = myObj.nextDouble();
+
+        // Output input by user
+        System.out.println("Name: " + name);
+        System.out.println("Height: " + height);
+        System.out.println("Vertical: " + vertical);
+
+        //Else0f Statements
+        if(height >= 68 && vertical > 35){
+            System.out.println("Ayo, "+ name + " You can dunk! Lets go!");
+        }
+
+        else if(height >= 60 && vertical > 45){
+            System.out.println("Ayo, "+ name + " You can dunk! Lets go!")
+            ;
+        }
+
+        else if(height <=60 && vertical < 45){
+            System.out.println("YO, yo, yo, " + name + "You can't dunk, you trash as hell!");
+        }
+
+        else if(height >=68 && vertical <= 35){
+            System.out.println("YO, yo, yo, " + name + "You can't dunk, you trash as hell!");
+        }
+    }
+}
