29313c804d515f33b427fc51d38e8c5a107140fd
Uhpachee <sea7wa@gmail.com>
2021-10-11 08:56:28 -0700

    put some rgb but it doesnt work

--- src/main/java/com/example/sping_portfolio/controllers/Combined.java
+++ src/main/java/com/example/sping_portfolio/controllers/Combined.java
@@ -6,7 +6,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -112,6 +119,39 @@ public class Combined {
     @GetMapping("/test")
     public String navbartest() {return "navbartest"; }
 
+    @GetMapping("/rgb")
+    public String rgb(Model model)  {
+        String web_server = "https://localhost:8080";
+        List<ImageInfo> lii = new ArrayList<>();
+
+        String file0 = "/images/rock.png";
+        lii.add(new ImageInfo(file0, web_server+file0, 3));
+        lii.get(0).read_image();
+
+        String file1 = "/images/dream.png";
+        lii.add(new ImageInfo(file1, web_server+file1, 3));
+        lii.get(1).read_image();
+
+        String file2 = "/images/flower.png";
+        lii.add(new ImageInfo(file2, web_server+file2, 3));
+        lii.get(2).read_image();
+
+        model.addAttribute("lii", lii);
+        return "rgb";
+    }
+
+    @GetMapping("/grayscaleimage")
+    public String grayscaleimage(Model model) {
+        String web_server = "http://localhost:8080/";
+        List<ImageInfo> lii = new ArrayList<>();
+
+        String file0 = "/images/rock.png";
+        lii.add(new ImageInfo(file0, web_server+file0, 12));
+        String str = lii.get(0).grayscale();
+//        String str = lii.get(0).grayscale();
+        model.addAttribute("str", str);
+        return "grayscaleimage";
+    }
 
 }
 
